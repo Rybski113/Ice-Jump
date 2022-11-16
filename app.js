9d3f341b45e876465939fc0ac175442e793c4687
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-11-16 21:26:14 +0100

    controls

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const grid  = document.querySelector('.grid')
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50;
-    let doodlerBottomSpace = 250;
+    let doodlerBottomSpace = 150;
     let isGameOver = false;
     let platformCount = 5;
     let platforms = [];
+    let upTimerId;
+    let downTimerId;
 
     function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add('doodler')
+        doodlerLeftSpace = platforms[0].left
         doodler.style.left = doodlerLeftSpace + 'px'
         doodler.style.bottom = doodlerBottomSpace + 'px'
     }
@@ -44,15 +47,56 @@ document.addEventListener('DOMContentLoaded', ()=> {
                 platform.bottom -= 4
                 let visual = platform.visual
                 visual.style.bottom = platform.bottom + 'px'
+                
             })
+        }
+    }
+
+    function jump() {
+        clearInterval(downTimerId)
+        upTimerId = setInterval(function() {
+            doodlerBottomSpace += 20
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace > 350) {
+                fall()
+            }
+        }, 30)
+    }
+
+    function fall() {
+        clearInterval(upTimerId)
+        downTimerId = setInterval(function() {
+            doodlerBottomSpace -= 5
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if ( doodlerBottomSpace <= 0) {
+               gameOver()
+            }
+        }, 30)
+    }
+
+    function gameOver() {
+        console.log('game over')
+        isGameOver = true;
+        clearInterval(upTimerId)
+        clearInterval(downTimerId)
+    }
+
+    function control(e) {
+        if (e.key === 'ArrowLeft') {
+            // move left
+        } else if (e.key === "ArrowRight") {
+            // move right
+        } else if (e.key === "ArrowUp") {
+            //move straight
         }
     }
 
     function start() {
         if(!isGameOver) {
-            createDoodler()
-            createPlatforms() 
+            createPlatforms()
+            createDoodler() 
             setInterval(movePlatforms, 30)
+            jump()
         }
     }
     //attach to button later
