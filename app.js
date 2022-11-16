8fba201b1b842d7ef1cf42eafaf72dd99deff814
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-11-16 19:51:57 +0100

    platforms added
    using class and constructor method

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50;
     let doodlerBottomSpace = 150;
-    let isGameOver = false
+    let isGameOver = false;
+    let platformCount = 5;
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -11,11 +12,37 @@ document.addEventListener('DOMContentLoaded', ()=> {
         doodler.style.left = doodlerLeftSpace + 'px'
         doodler.style.bottom = doodlerBottomSpace + 'px'
     }
+
+    class Platform {
+        constructor(newPlatformBottom) {
+            this.bottom = newPlatformBottom;
+            this.left = Math.random() * 315;
+            this.visual = document.createElement('div');
+
+            const visual = this.visual;
+            visual.classList.add('platform');
+            visual.style.left = this.left + 'px';
+            visual.style.bottom = this.bottom + 'px';
+            grid.appendChild(visual)
+        }
+    }
+
+    function createPlatforms() {
+        for(let i=0; i < platformCount; i++) {
+           let platformGap = 600 / platformCount
+           let newPlatformBottom = 100 + i * platformGap
+           let newPlatform = new Platform(newPlatformBottom)
+        }
+    }
    
 
     function start() {
-        if(isGameOver == false) {
+        if(!isGameOver) {
             createDoodler()
+            createPlatforms() 
+            
         }
     }
+    //attach to button later
+    start()
 })
